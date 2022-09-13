475592363a196a9d1dd5d656816887f97fa65113
AREEG94FAHAD <30151596+AREEG94FAHAD@users.noreply.github.com>
2022-09-13 14:01:52 +0300

    Create 10bool.c

---
+++ 10bool.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    int num1 = 10;
+    int num2 = 20;
+
+    bool value = (num1 == 2) && (num2==4);
+
+    printf("%d",value);
+
+    return 0;
+}
